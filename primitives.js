17a4ddab166da358daf5908c915a9d9ab21f6577
kevnroberts <kevnroberts@gmail.cm>
2012-08-29 22:21:28 -0600

    adding initial tests for "cons"

--- primitives.js
+++ primitives.js
@@ -1,16 +1,18 @@
 // functions native to Scheme
 
 function car(arr){
-	if(arr.constructor === Array && arr.length){
+	if(arr instanceof Array && arr.length){
 		return arr[0];
 	}
 	return null;
 }
 
 function cdr(arr){
-	if(!arr.length) return;
-	arr.shift();
-	return arr;
+	if(arr instanceof Array && arr.length){
+		arr.shift();
+		return arr;
+	}
+	return null;
 }
 
 function cons(obj, arr){
--- tests/primitives.js
+++ tests/primitives.js
@@ -5,7 +5,7 @@ describe("Primitives", function(){
 			expect(result).toEqual('a');
 		});
 
-		it("should return ['a', 'b', 'c'] when param is [['a', 'b', 'c'], 'x', y', 'z']", function(){
+		it("should return ['a', 'b', 'c'] when param is [['a', 'b', 'c'], 'x', 'y', 'z']", function(){
 			var subArr = ['a', 'b', 'c'];
 			var arr = [subArr, 'x', 'y', 'z'];
 			var result = car(arr);
@@ -18,6 +18,71 @@ describe("Primitives", function(){
 
 		it("should return 'null' when param is []", function(){
 			expect(car([])).toBe(null);
+		});
+	});
+
+	describe("cdr", function(){
+		it("should return ['b', 'c'] when param is ['a', 'b', 'c']", function(){
+			var result = cdr(['a', 'b', 'c']);
+			expect(result.length).toEqual(2);
+			expect(result[0]).toEqual('b');
+			expect(result[1]).toEqual('c');
+		});
+		it("should return ['x', 'y', 'z'] when param is [['a', 'b', 'c'], 'x', 'y', 'z']", function(){
+			var result = cdr([['a', 'b', 'c'], 'x', 'y', 'z']);
+			expect(result.length).toEqual(3);
+			expect(result[0]).toEqual('x');
+			expect(result[1]).toEqual('y');
+			expect(result[2]).toEqual('z');
+		});
+		it("should return [] when param is ['hamburger']", function(){
+			var result = cdr(['hamburger']);
+			expect(result.length).toEqual(0);
+		});
+		it("should return 'null' when param is 'hotdogs'", function(){
+			var result = cdr('hotdogs');
+			expect(result).toBe(null);
+		});
+		it("should return 'null' when param is []", function(){
+			var result = cdr([]);
+			expect(result).toBe(null);
+		});
+
+		describe("cdr with car", function(){
+			it("should return ['x', 'y'] when calling car(cdr([['b'], ['x', 'y'], [['c']]]))", function(){
+				var result = car(cdr([['b'], ['x', 'y'], [['c']]]));
+				expect(result.constructor).toEqual(Array);
+				expect(result.length).toEqual(2);
+				expect(result[0]).toEqual('x');
+				expect(result[1]).toEqual('y');
+			});
+			it("should return [[['c']]] when calling cdr(cdr([['b'], ['x', 'y'], [['c']]]))", function(){
+				var result = cdr(cdr([['b'], ['x', 'y'], [['c']]]));
+				expect(result instanceof Array).toEqual(true);
+				expect(result.length).toEqual(1);
+				var sub1 = result[0];
+				expect(sub1 instanceof Array).toEqual(true);
+				expect(sub1.length).toEqual(1);
+				var sub2 = sub1[0];
+				expect(sub2 instanceof Array).toEqual(true);
+				expect(sub2.length).toEqual(1);
+				expect(sub2[0]).toEqual('c');
+			});
+			it("should return 'null' when calling cdr(car(['a', ['b', ['c']], 'd']))", function(){
+				var result = cdr(car(['a', ['b', ['c']], 'd']));
+				expect(result).toBe(null);
+			});
+		});
+	});
+
+	describe("cons", function(){
+		it("should return ['peanut', 'butter', 'and', 'jelly'] when calling cons('peanut', ['butter', 'and', 'jelly'])", function(){
+			var result = cons('peanut', ['butter', 'and', 'jelly']);
+			expect(result.length).toEqual(4);
+			expect(result[0]).toEqual('peanut');
+			expect(result[1]).toEqual('butter');
+			expect(result[2]).toEqual('and');
+			expect(result[3]).toEqual('jelly');
 		});
 	});
 });
